0e99d3c87c9c853969fea78ccd5f82e93dcc452d
Vlademir <vlademir.monteiro@hotmail.com>
2024-07-21 15:08:12 -0300

    hope this works

---
+++ trial.R
@@ -0,0 +1,8 @@
+mena_countries <- c("Algeria", "Bahrain", "Egypt", "Iran", "Iraq", "Israel", "Jordan", "Kuwait", "Lebanon", "Libya", "Morocco", "Oman",  "Qatar", "Saudi Arabia", "Syria", "Tunisia", "United Arab Emirates", "Yemen")
+
+trade <- file_excel %>%
+  pivot_longer(cols = 6:17,
+               names_to = "year" , 
+               values_to = "value_exportation") %>%
+  filter(Partner_Name %in% mena_countries) %>%
+  filter(year < 2012) 
